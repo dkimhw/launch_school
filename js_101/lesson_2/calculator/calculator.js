e1b71e5231b38e1140f482c1eb94f4e134768fb4
David Kim <dkhw90@gmail.com>
2020-03-10 03:17:24 -0400

    Enable eslint

--- js_101/lesson_2/calculator/calculator.js
+++ js_101/lesson_2/calculator/calculator.js
@@ -6,32 +6,69 @@
 // Perform the operation on the two numbers
 // Print the result to the terminal
 
+// ./node_modules/.bin/eslint js_101/lesson_2/calculator/calculator.js
+
 let readline = require('readline-sync');
 
-console.log("Welcome to Calculator");
+// Create a prompt function - creates a more distinctive output for questions:
+function prompt(msg) {
+    console.log(`=> ${msg}`);
+}
+
+prompt("Welcome to Calculator");
 
 // Ask for the first number
-console.log("What's the first number?");
-let number1 = Number(readline.question());
+prompt("What's the first number?");
+let number1 = readline.question();
+
+while (invalidNumber(number1)) {
+    prompt("Not a valid number.");
+    number1 = readline.question();
+}
 
 // Ask for the second number
-console.log("What's the second number?");
-let number2 = Number(readline.question());
+prompt("What's the second number?");
+let number2 = readline.question();
+
+while (invalidNumber(number2)) {
+    prompt("Not a valid number.");
+    number2 = readline.question();
+}
+
+function invalidNumber(number) {
+    // Number(number) - returns NaN if for example 'bob' was passed in
+    // Number.isNaN - true if the argument passed in is NaN
+    return Number.isNaN(Number(number));
+}
 
 // Ask for operation to perform
-console.log("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide")
+prompt("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide");
 let operation = readline.question();
+
+while (!['1', '2', '3', '4'].includes(operation)) {
+    prompt("Must choose 1, 2, 3, or 4");
+    operation = readline.question();
+}
+
+// Convert the number1 and number 2 to numbers
+number1 = Number(number1);
+number2 = Number(number2);
 
 // Perform the operation user asked for
 let output;
-if (operation === '1') {
-    output = number1 + number2;
-} else if (operation === '2') {
-    output = number1 - number2;
-} else if (operation === '3') {
-    output = number1 * number2;
-} else if (operation === '4') {
-    output = number1 / number2;
+switch (operation) {
+    case '1':
+        output = number1 + number2;
+        break;
+    case '2':
+        output = number1 - number2;
+        break;
+    case '3':
+        output = number1 * number2;
+        break;
+    case '4':
+        output = number1 / number2;
+        break;
 }
 
-console.log(`The result is: ${output}`);
+prompt(`The result is: ${output}`);
